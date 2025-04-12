05ed28f06aa561870a54178d2ce71689b3c0f80c
mayur20022 <mtalewar2002@gmail.com>
2025-04-12 13:53:59 +0530

    completed

--- Backend/server.js
+++ Backend/server.js
@@ -11,7 +11,10 @@ const farmerRoute = require('./router/farmerRoute')
 db()
 const port = process.env.PORT || 3000
 
-app.use(cors());
+app.use(cors({
+    origin: 'https://scp-manager-frontend.onrender.com',
+    credentials: true,
+}));
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser())
