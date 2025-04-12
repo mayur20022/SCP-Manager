95bdb71f4826d26729c1f699c88f1e6a0b09b8c8
mayur20022 <mtalewar2002@gmail.com>
2025-04-12 13:46:34 +0530

    completed

--- Backend/server.js
+++ Backend/server.js
@@ -21,6 +21,9 @@ app.use(cookieParser())
 
 app.use('/scp', scpRoute)
 app.use('/farmer', farmerRoute)
+app.get("/", (req, res) => {
+    res.send("SCP Manager API")
+})
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`)
