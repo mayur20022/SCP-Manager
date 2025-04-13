f7429c95779721fbf39a99a52b60e72621c40da9
Mayuresh Talewar <125124658+mayur20022@users.noreply.github.com>
2025-04-13 11:40:59 +0530

    Update scpModel.js

--- Backend/models/scpModel.js
+++ Backend/models/scpModel.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
     name: { type: String, required: true },
-    email: { type: String },
+    email: { type: String, required: true },
     phone: { type: String, required: true, match: /^[0-9]{10}$/ },
     password: { type: String, required: true, minlength: 12 },
 });
