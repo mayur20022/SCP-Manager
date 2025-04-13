42ad86ccba1950587f46dcf43735cc9070f5541d
Mayuresh Talewar <125124658+mayur20022@users.noreply.github.com>
2025-04-13 11:42:07 +0530

    Update farmerModel.js

--- Backend/models/farmerModel.js
+++ Backend/models/farmerModel.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 
 const farmerSchema = new mongoose.Schema({
-    scpUserId: { type: mongoose.Schema.Types.ObjectId, ref: 'Scp' },
     name: { type: String, required: true },
     phone: { type: String, required: true, match: /^[0-9]{10}$/ },
     village: String,
